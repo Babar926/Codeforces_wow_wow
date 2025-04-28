9e9ed0be3a4a6e6f7c11ba3e975eee3f1cbd25ef
Jahir Uddin Mohammad Babar <jbabar926@gmail.com>
2025-04-28 20:32:10 +0600

    Add files via upload

---
+++ 550a.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    char s[100000000],*t;
+    cin>>s;
+    if((t=strstr(s,"AB"))&&(strstr(t+2,"BA")))
+    {
+        cout<<"YES"<<endl;
+    }
+    else if((t=strstr(s,"BA"))&&(strstr(t+2,"AB")))
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
